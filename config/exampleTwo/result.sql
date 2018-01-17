f0526c73aaec733c3440a8692e60088d743cf3f6
Kevin <kevindeeg@gmail.com>
2018-01-17 19:54:49 +0000

    almost right!

--- config/exampleTwo/result.sql
+++ config/exampleTwo/result.sql
@@ -11,11 +11,11 @@ CREATE TABLE `user1` (
 
 CREATE TABLE `user1_login` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int unsigned NOT NULL DEFAULT '0',
+  `user1_id` int unsigned NOT NULL DEFAULT '0',
   `modified_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
+  KEY `user1_id` (`user1_id`),
   CONSTRAINT `user1_login_ibfk_1` FOREIGN KEY (`user1_id`) REFERENCES `user1` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1_swedish_ci;
 
@@ -30,10 +30,10 @@ CREATE TABLE `user2` (
 
 CREATE TABLE `user2_login` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int unsigned NOT NULL DEFAULT '0',
+  `user2_id` int unsigned NOT NULL DEFAULT '0',
   `modified_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
+  KEY `user2_id` (`user2_id`),
   CONSTRAINT `user2_login_ibfk_1` FOREIGN KEY (`user2_id`) REFERENCES `user2` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=MyISM DEFAULT CHARSET=utf8;
